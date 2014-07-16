f4b04ce21f55b0f360296a492c013e347b59fba8
Ned Ruggeri <ruggeri@self-loop.com>
2014-07-15 22:48:29 -0700

    update Sub/SubsController/views

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,16 +1,14 @@
 class SubsController < ApplicationController
   before_action :require_signed_in!, except: [:index, :show]
-  before_action :sub_exists?, only: [:edit, :update, :show]
-  before_action :user_owns_sub?, only: [:edit, :update]
+  before_action :require_user_owns_sub!, only: [:edit, :update]
 
   def index
     @subs = Sub.all
+    render :index
   end
 
   def new
     @sub = Sub.new
-    5.times { @sub.links.new }
-
     render :new
   end
 
@@ -20,28 +18,16 @@ class SubsController < ApplicationController
 
   def create
     @sub = current_user.subs.new(sub_params)
-    filled_out_links = params[:links].values.reject do |value|
-      value[:url].empty? || value[:title].empty?
-    end
-
-    filled_out_links.each do |link_params|
-      @sub.links.new(link_params.merge(user_id: current_user.id))
-    end
-
-    # @sub.save automatically performs a transaction.
     if @sub.save
-      redirect_to @sub
+      redirect_to sub_url(@sub)
     else
-      # Ensures we have 5 links, regardless of how many links were filled out.
-      # Otherwise the :new page will only have as many link fields as filled_out_links
       flash.now[:errors] = @sub.errors.full_messages
-      (5 - @sub.links.length).times { @sub.links.new }
       render :new
     end
   end
 
   def update
-    if @sub.update_attributes(sub_params)
+    if sub.update(sub_params)
       redirect_to @sub
     else
       flash.now[:errors] = @sub.errors.full_messages
@@ -54,17 +40,16 @@ class SubsController < ApplicationController
   end
 
   private
-  def user_owns_sub?
-    redirect_to subs_url unless @sub.moderator == current_user
+  def require_user_owns_sub!
+    set_sub!
+    redirect_to subs_url unless sub.moderator == current_user
   end
 
-  def sub_exists?
-    @sub = Sub.includes(:links).find_by_id(params[:id])
-    redirect_to subs_url unless @sub
+  def set_sub!
+    @sub ||= Sub.includes(:links).find(params[:id])
   end
 
   def sub_params
-    params.require(:sub).permit(:name, :links => [:url, :title, :body])
+    params.require(:sub).permit(:name, links: [:url, :title, :body])
   end
 end
-
--- app/models/sub.rb
+++ app/models/sub.rb
@@ -1,13 +1,15 @@
 class Sub < ActiveRecord::Base
   validates :name, :moderator, presence: true
+  validates :name, uniqueness: true
 
   has_many :link_subs, inverse_of: :sub, dependent: :destroy
   has_many :links, through: :link_subs, source: :link
 
-  belongs_to :moderator,
-              class_name: "User",
-              foreign_key: :moderator_id,
-              primary_key: :id,
-              inverse_of: :subs
+  belongs_to(
+    :moderator,
+    class_name: "User",
+    foreign_key: :moderator_id,
+    primary_key: :id,
+    inverse_of: :subs
+  )
 end
-
