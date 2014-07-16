e15f2c81fd2b67672d09f76366fd03218eaab328
Ned Ruggeri <ruggeri@self-loop.com>
2014-07-15 23:28:25 -0700

    beef up validations/constraints on UserVote

--- app/models/user_vote.rb
+++ app/models/user_vote.rb
@@ -1,7 +1,8 @@
 class UserVote < ActiveRecord::Base
   validates :user, :link, presence: true
+  # don't let the user vote twice!
+  validates :link_id, uniqueness: { scope: :user_id}
 
   belongs_to :user, inverse_of: :user_votes
   belongs_to :link, inverse_of: :user_votes
 end
-
--- db/migrate/20131117152902_create_user_votes.rb
+++ db/migrate/20131117152902_create_user_votes.rb
@@ -1,10 +1,13 @@
 class CreateUserVotes < ActiveRecord::Migration
   def change
     create_table :user_votes do |t|
-      t.integer :user_id
-      t.integer :link_id
-      t.integer :value
+      t.integer :user_id, null: false
+      t.integer :link_id, null: false
+      t.integer :value, null: false
       t.timestamps
     end
+
+    add_index :user_votes, :link_id
+    add_index :user_votes, [:link_id, :user_id], uniqueness: true
   end
 end
